a42cccf7e1863ec0bd5ada2edd55542a1aef7c7c
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2018-04-29 13:35:51 +0300

    Add 2.6

---
+++ puhelinluettelo/src/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+const Person = (props) => {
+  return (
+    <div>{props.name}</div>
+  )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  addPerson = (event) => {
+    event.preventDefault()
+
+    const newPerson = {
+      name: this.state.newName
+    }
+
+    this.setState((prevState) => {
+      const persons = prevState.persons.concat(newPerson)
+      return {
+        persons,
+        newName: ''
+      }
+    })
+  }
+
+  updateNewName = (event) => {
+    this.setState({ newName: event.target.value })
+  }
+
+  render() {
+    const names = this.state.persons.map(person => <Person key={person.name} name={person.name} />)
+
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addPerson}>
+          <div>
+            nimi:
+            <input
+              value={this.state.newName}
+              onChange={this.updateNewName}
+              />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        {names}
+      </div>
+    )
+  }
+}
+
+export default App
